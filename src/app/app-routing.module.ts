7065082fa74881acbd2c0134b3e54b31395af9f5
Eugene Derienko <Derienkod194@gmail.com>
2019-03-12 21:51:39 +0200

    Add login and main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './loginPage/login-page/login-page.component';
+import { MainPageComponent } from './mainPage/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: '', component: MainPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserService } from 'src/api/user.service';
+import { LoginPageComponent } from './loginPage/login-page/login-page.component';
+import { MainPageComponent } from './mainPage/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
